4d0217e36af82a5b9d64922f07384be82c03c02f
Roman Koff <krv@guap.ru>
2026-01-29 15:39:26 +0300

    Update Edit_Reference.cs

--- Forms/~edit/Edit_Reference.cs
+++ Forms/~edit/Edit_Reference.cs
@@ -11,33 +11,33 @@ namespace Ans.Net10.Web.Forms
 		public Edit_Reference(
 			string name,
 			int? value,
-			RegistryList registry,
-			RegistryModeEnum mode,
-			string cssClasses)
+			RegistryList registry = null,
+			RegistryModeEnum registryMode = RegistryModeEnum.Auto,
+			string cssClasses = null)
 			: base(
 				  name,
 				  value?.ToString(),
 				  registry,
-				  mode,
+				  registryMode,
 				  cssClasses,
 				  false)
 		{
 		}
 
 
-		public Edit_Reference(
-			string name,
-			int? value,
-			RegistryList registry,
-			string cssClasses = null)
-			: this(
-				  name,
-				  value,
-				  registry,
-				  RegistryModeEnum.Auto,
-				  cssClasses)
-		{
-		}
+		//public Edit_Reference(
+		//	string name,
+		//	int? value,
+		//	RegistryList registry,
+		//	string cssClasses = null)
+		//	: this(
+		//		  name,
+		//		  value,
+		//		  registry,
+		//		  RegistryModeEnum.Auto,
+		//		  cssClasses)
+		//{
+		//}
 
 	}
 
